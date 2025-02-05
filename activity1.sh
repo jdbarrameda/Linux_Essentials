36eb5caddc866f3dae6d4ccff40f88f45fb8e19c
jdbarrameda <133651151+jdbarrameda@users.noreply.github.com>
2025-02-05 01:46:25 +0000

    done

---
+++ activity1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter Name:" 
+read NAME
+
+echo "Enter Age:"
+read AGE
+
+echo "Enter Program:"
+read PROGRAM
+
+echo "Hello, $NAME! You are $AGE years old & your program is $PROGRAM."
---
+++ sample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Declare a variable
+USER_NAME="Student"
+
+# Print a greeting using the variable
+echo "HELLO, $USER_NAME! Welcome to Bash scripting."
+
+# Ask for user input
+echo "Enter your name:"
+read USER_NAME
+
+# Display a personalized message
+echo "Nice to meet you, $USER_NAME!"
+
+
